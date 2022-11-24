66c1c7f3a8bdd31dd868bf12c9d07007aa227b28
선건 <tjsrjs21@gmail.com>
2022-11-24 16:40:09 +0900

    만들어봄

--- codingtest.js
+++ codingtest.js
@@ -1,3 +1,19 @@
-sdds;
-dsdfsf;
-dsdf;
+//몫 구하기
+//정수 num1, num2가 매개변수로 주어질 때, num1을 num2로 나눈 몫을 return 하도록 solution 함수를 완성해주세요.
+const solution = (num1, num2) => parseInt(num1 / num2);
+
+//두수의 곱
+//정수 num1, num2가 매개변수 주어집니다. num1과 num2를 곱한 값을 return 하도록 solution 함수를 완성해주세요.
+const solution = (num1, num2) => num1 * num2;
+
+//두수의 차
+//정수 num1과 num2가 주어질 때, num1에서 num2를 뺀 값을 return하도록 soltuion 함수를 완성해주세요.
+const solution = (num1, num2) => num1 - num2;
+
+//나머지 구하기
+//정수 num1, num2가 매개변수로 주어질 때, num1를 num2로 나눈 나머지를 return 하도록 solution 함수를 완성해주세요.
+const solution = (num1, num2) => num1 % num2;
+
+//나이출력
+//머쓱이는 40살인 선생님이 몇 년도에 태어났는지 궁금해졌습니다. 나이 age가 주어질 때, 2022년을 기준 출생 연도를 return 하는 solution 함수를 완성해주세요.
+const solution = (age) => 2023 - age;
