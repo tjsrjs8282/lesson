4bc57469d75335da3901bb5430a9fc70bfd6f386
선건 <tjsrjs21@gmail.com>
2022-12-06 16:17:55 +0900

    20221206 코테

--- codingtest3.js
+++ codingtest3.js
@@ -164,10 +164,10 @@ function solution(n) {
   let answer = [];
   // 1부터 n까지 반복하며 모든 약수를 정답 배열에 입력
   for (let i = 1; i <= n; i++) {
-    if (n % i === 0) result.push(i);
+    if (n % i === 0) answer.push(i);
   }
   // 모든 약수의 수는 순서쌍의 수와 같음
-  return result.length;
+  return answer.length;
 }
 
 //짝수는 싫어요
