d2ba238af365a09f7809ab4543d5c00448f8cb04
seongeon <dbtjsrjs8282@naver.com>
2023-02-05 21:27:26 +0900

    codingtest 3개 추가

--- coding_test/codingTest11.js
+++ coding_test/codingTest11.js
@@ -63,3 +63,15 @@ function solution(a, b) {
 //영어 점수와 수학 점수의 평균 점수를 기준으로 학생들의 등수를 매기려고 합니다.
 //영어 점수와 수학 점수를 담은 2차원 정수 배열 score가 주어질 때,
 // 영어 점수와 수학 점수의 평균을 기준으로 매긴 등수를 담은 배열을 return하도록 solution 함수를 완성해주세요.
+
+function solution(score) {
+  let arr = score.map(([a, b]) => (a + b) / 2);
+  let a = [...arr].sort((a, b) => b - a);
+  return arr.map((v) => a.indexOf(v) + 1);
+}
+
+//평균값 구하기 = 요소안의 a b를 더하고 2로 나눈다.
+//오름차순으로 만들기
+//평균값 구한 배열을 돌려서 indexOf 로 인덱스찾기 중복되는게 있으면 가장 앞에 index값 가져옴
+//let a = arr.sort((a, b) => b - a);
+// slice()를 사용해서 복사가능함
